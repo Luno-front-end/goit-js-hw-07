2eb92f739c0a9d4f79917e36577ebff012dfd655
Maksym Zaika <Onlyrobochaposhta@gmail.com>
2020-09-23 08:55:26 +0300

    Update task-5.js

--- task-5.js
+++ task-5.js
@@ -4,5 +4,9 @@ const outputEl = document.querySelector("#name-output");
 inputEl.addEventListener("input", onInput);
 
 function onInput(event) {
-  outputEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value) {
+    outputEl.textContent = event.currentTarget.value;
+  } else {
+    outputEl.textContent = "незнакомец";
+  }
 }
